34ac2a48eac1133020230850f0ac7e3921ec51b8
Grzegorz Świderski <grzegorzswiderski1988@gmail.com>
2017-07-06 19:29:13 +0200

    plus or minus

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,28 @@
 ﻿// scripts.js
 
+var numberA = prompt('Number A');
+console.log('Number A= ' + numberA);
+
+var numberB = prompt('Number B');
+console.log('Number B= ' + numberB);
+	
+var value = (numberA * numberA)+(2 * numberA * numberB)-(numberB * numberB);
+alert('Wynik działania= ' + value);
+console.log('Wynik działania= ' + value);
+	
+if (value > 0) {
+console.log('Wynik dodatni');
+alert('Wynik dodatni');
+}
+ 
+else if (value < 0) {
+console.log('Wynik ujemny');
+alert('Wynik ujemny');
+}
+
+else {
+console.log('Ani dodatni, ani ujemny:)');
+alert('Ani dodatni, ani ujemny:)');
+}
+
 
