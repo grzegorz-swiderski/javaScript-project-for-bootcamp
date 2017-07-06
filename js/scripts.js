7870e203f2fe8378da625d369461304c1bb5e3cb
Grzegorz Świderski <grzegorzswiderski1988@gmail.com>
2017-07-06 19:22:10 +0200

    triangle field

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,13 @@
 ﻿// scripts.js
 
+var sideA = prompt('Value A');
+console.log('Value A= ' + sideA);
+
+var hightH = prompt('value H');
+console.log('value H= ' + hightH);
+
+var triangleArea = sideA * hightH / 2;
+alert('Value triangle field= ' + sideA * hightH / 2);
+console.log('Triangle field with base a: ' + sideA + ' and height h: ' + hightH + ' is equal to: ' + triangleArea);	
+
 
