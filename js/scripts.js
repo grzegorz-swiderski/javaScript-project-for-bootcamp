dff94fd378861b5728acd62c7ef079c11aaff4d2
Grzegorz Świderski <grzegorzswiderski1988@gmail.com>
2017-07-08 00:41:44 +0200

    use ternary

--- js/scripts.js
+++ js/scripts.js
@@ -10,19 +10,31 @@ var value = (numberA * numberA) + (2 * numberA * numberB) - (numberB * numberB);
 alert('Wynik działania= ' + value);
 console.log('Wynik działania= ' + value);
 	
-if (value > 0) {
-console.log('Wynik dodatni');
-alert('Wynik dodatni');
+/*if (value > 0) {
+	console.log('Wynik dodatni');
+	alert('Wynik dodatni');
 }
  
 else if (value < 0) {
-console.log('Wynik ujemny');
-alert('Wynik ujemny');
+	console.log('Wynik ujemny');
+	alert('Wynik ujemny');
 }
 
 else {
-console.log('Ani dodatni, ani ujemny:)');
-alert('Ani dodatni, ani ujemny:)');
+	console.log('Ani dodatni, ani ujemny:)');
+	alert('Ani dodatni, ani ujemny:)');
+}*/
+
+var valueName = "Wynik" + ((value > 0) ?  " dodatni." : " ujemny.");
+
+if (value == 0) {
+	console.log('Ani dodatni, ani ujemny:)');
+	alert('Ani dodatni, ani ujemny:)');
+}
+
+else {
+	alert(' ' + valueName);
+	console.log(' ' + valueName); 
 }
 
 
