d57116d74870d4d78e7bf6983b1f13dc56544c2f
Grzegorz Świderski <grzegorzswiderski1988@gmail.com>
2017-07-08 00:08:45 +0200

    triangle field

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,25 @@
 ﻿// scripts.js
 
+var numberA = prompt('Number A');
+	console.log('Number A= ' + numberA);
+	numberH = prompt('Number H');
+	console.log('Number H= ' + numberH);
 
+
+
+function getTriangleArea(numberA, numberH) {
+	if ((numberA <= 0) || (numberH <= 0)){
+	alert('Nieprawdziwe dane');
+	return ('Nieprawdziwe dane');}
+	
+	else if ((numberA > 0) || (numberH > 0)){
+	alert('Równa się= ' + (numberA * numberH / 2));
+	return numberA * numberH / 2;}
+}
+
+console.log( getTriangleArea(numberA,numberH) )
+
+/*var triangle1Area = getTriangleArea(10, 15);
+var triangle2Area = getTriangleArea(30, 2);
+var triangle3Area = getTriangleArea(8, 13);*/
+	
