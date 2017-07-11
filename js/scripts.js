1973ed51bf3b3d6825f3a2b344a94e44af04f47b
Grzegorz Świderski <grzegorzswiderski1988@gmail.com>
2017-07-11 18:16:11 +0200

    list

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,14 @@
 ﻿// scripts.js
+var list = document.getElementById('list');
+var add = document.getElementById('addElem');
+
+add.addEventListener('click', function(e) {
+	var newElem = document.createElement('li');
+	newElem.innerHTML = 'item';
+	
+	newElem.getElementsByTagName('li').length;
+	
+	list.appendChild(newElem);
+});
 
 
