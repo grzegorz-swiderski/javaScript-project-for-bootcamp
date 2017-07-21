f3413c92a53778bafcc0dd880d608ad83543cd31
Grzegorz Świderski <grzegorzswiderski1988@gmail.com>
2017-07-21 21:01:18 +0200

    little change - new color

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,34 @@
 ﻿// scripts.js
+$(document).ready(function(){
 
-var span = $('span');
-    span.each(function(index, element) {
+	var span = $('span');
+	var ignored = ['#sp1', '#sp2', '#sp4'];
+	console.log(ignored);
+
+
+	    
+	span.each(function(index, element) {
 		if(index % 2 == 0){
 			$(element).css('color', 'red')
-		};
-});
-
-var paragraphs = $('p');
-paragraphs.each(function(index, element) {
+		}
+	});	
 	
-	var button = '<button class="btn" data-tmp="' + index + '">Click me</button>'
-	$(element).append(button)
+	    
+	ignored.forEach(function(element) {
+    	console.log(element);
+    	$(element).css('color', 'black')
+	});
+
+	var paragraphs = $('p');
+	paragraphs.each(function(index, element) {
+		
+		var button = '<button class="btn" data-tmp="' + index + '">Click me</button>'
+		$(element).append(button)
+		
+	});
 	
-});
+	$("button").click(function(){
+		alert($(this).attr("data-tmp"));
+	});
 
-$("button").click(function(){
-	alert($(this).attr("data-tmp"));
 });
-
