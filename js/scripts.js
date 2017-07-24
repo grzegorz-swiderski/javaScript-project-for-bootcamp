ee29951954de8b3cf970ab09c2b286560947a4d4
Grzegorz Świderski <grzegorzswiderski1988@gmail.com>
2017-07-24 21:22:40 +0200

    use self

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,22 @@
 ﻿// scripts.js
 
+function Button(text) {
+	this.text = text || 'Hello';
+}
 
+console.log(Button);
+
+Button.prototype = {
+	create: function() {
+		var self = this;
+		this.$element = $('<button>');
+		this.$element.text(this.text);
+		this.$element.click(function() {
+			alert(self.text);
+		});
+		this.$element.appendTo($('body'));
+	}
+}
+
+var btn1 = new Button('Hello!');
+btn1.create();
