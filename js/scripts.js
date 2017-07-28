abf432e7acb2a329eb141f35f74fc3a7dcf517ca
Grzegorz Świderski <grzegorzswiderski1988@gmail.com>
2017-07-28 02:16:04 +0200

    getWarrany change

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,9 @@ function Phone(brand, price, color, country) {
 	this.price = price;
 	this.color = color;
 	this.country = country;
+	this.getWarranyCost = function(){
+		console.log("The phone brand is " + this.brand + ", color is " + this.color + "and the price is " + this.price + " zł. It is from " +this.country + ". WarranyCost is " + this.price * 0.1);
+	};
 }
 
 Phone.prototype.printInfo = function(){
@@ -18,21 +21,4 @@ var onePlusOne = new Phone ("OnePlus", 1899, "white", "China");
 
 samsungGalaxyS6.printInfo();
 
-function getWarranyCost(brand, price, color, country, warranyCost) {
-	Phone.call(this, brand, price, color, country);
-	this.warranyCost = this.price * 0.1;
-}
-
-getWarranyCost.prototype = Object.create(Phone.prototype)
-getWarranyCost.prototype.constructor = getWarranyCost;
-
-getWarranyCost.prototype.printInfo = function(){
-console.log("The phone brand is " + this.brand + ", color is " + this.color + "and the price is " + this.price + " zł. It is from " + this.country + ". WarranyCost for this phone is " + this. warranyCost + ".");
-}
-
-var samsungGalaxyS6 = new getWarranyCost ("Samsung", 2599.9, "black", "South Korea");
-var iPhone6S = new getWarranyCost("Apple", 2250, "silver", "United States of America");
-var onePlusOne = new getWarranyCost ("OnePlus", 1899, "white", "China");
-
-iPhone6S.printInfo();
-
+iPhone6S.getWarranyCost();
